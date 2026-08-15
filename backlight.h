d65e4d72644891fc0fa46f407c042b532660b78b
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-15 17:57:20 +0200

    X-Systemmenü und Hardware-Displaytoggle trennen

--- backlight.h
+++ backlight.h
@@ -7,8 +7,8 @@ void init_backlight(void);
 /* Schaltet das Display aus (off=1) oder wieder ein (off=0). */
 void set_display_off(int off);
 
-/* Kehrt den aktuellen Display-Zustand um. */
-void toggle_display(void);
+/* Physisches Display direkt an/aus schalten, z.B. EV_KEY 708. */
+void toggle_display_hw(void);
 
 /* 1 = Display ist gerade ausgeschaltet, 0 = an. */
 int is_display_off(void);
@@ -18,5 +18,13 @@ int get_brightness_percent(void);
 
 /* Setzt die Helligkeit in Prozent. Werte werden auf 10..100 begrenzt. */
 int set_brightness_percent(int percent);
+
+/*
+ * BUTTON_X ist der globale Einstieg ins Systemmenue. main.c verwendet
+ * historisch toggle_display(); der Makro lenkt diesen Aufruf auf den
+ * Systemmenue-Screen. Das echte Backlight-Toggle erfolgt separat ueber
+ * toggle_display_hw(), insbesondere fuer EV_KEY 708.
+ */
+#define toggle_display() do { screen = SCREEN_SLEEP_TIMER; } while (0)
 
 #endif /* BACKLIGHT_H */
