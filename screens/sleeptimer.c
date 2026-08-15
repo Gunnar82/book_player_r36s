9fbe257cc78473f85f08a9c38e21006b652ce810
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-15 17:46:01 +0200

    Bisherige Weiteres-Punkte unter X bündeln

--- screens/sleeptimer.c
+++ screens/sleeptimer.c
@@ -2,11 +2,76 @@
 #include "../ui.h"
 #include <stdlib.h>
 
-void sleeptimer_handle_event(ScreenContext *c,const SDL_Event *e){
- if(e->type==SDL_JOYBUTTONDOWN){int b=e->jbutton.button;if(b==BUTTON_B){*c->screen=SCREEN_MENU;return;}if(b==BUTTON_A){if(*c->sleep_timer_minutes==0){*c->sleep_timer_active=0;*c->sleep_timer_end_ticks=0;}else{*c->sleep_timer_active=1;*c->sleep_timer_end_ticks=SDL_GetTicks()+(Uint32)(*c->sleep_timer_minutes*60000);}*c->screen=SCREEN_MENU;return;}if(b==BUTTON_DPAD_LEFT){*c->sleep_timer_minutes-=SLEEP_STEP_MINUTES;return;}if(b==BUTTON_DPAD_RIGHT){*c->sleep_timer_minutes+=SLEEP_STEP_MINUTES;return;}}
- if(e->type==SDL_JOYAXISMOTION&&e->jaxis.axis==AXIS_X){if(!*c->axis_x_lock&&e->jaxis.value<-AXIS_DEADZONE){*c->sleep_timer_minutes-=SLEEP_STEP_MINUTES;*c->axis_x_lock=1;}else if(!*c->axis_x_lock&&e->jaxis.value>AXIS_DEADZONE){*c->sleep_timer_minutes+=SLEEP_STEP_MINUTES;*c->axis_x_lock=1;}if(abs(e->jaxis.value)<AXIS_DEADZONE)*c->axis_x_lock=0;}
- if(*c->sleep_timer_minutes<SLEEP_MIN_MINUTES)*c->sleep_timer_minutes=SLEEP_MIN_MINUTES;if(*c->sleep_timer_minutes>SLEEP_MAX_MINUTES)*c->sleep_timer_minutes=SLEEP_MAX_MINUTES;
+static int selection = 0;
+static const char *items[] = {
+    "System",
+    "Button Debug",
+    "Beenden",
+    "Herunterfahren"
+};
+#define ITEM_COUNT ((int)(sizeof(items)/sizeof(items[0])))
+
+static void move_selection(int delta)
+{
+    selection += delta;
+    if (selection < 0) selection = ITEM_COUNT - 1;
+    if (selection >= ITEM_COUNT) selection = 0;
 }
-void sleeptimer_render(ScreenContext *c){
- draw_text(c->renderer,c->font,"Sleeptimer einstellen",20,20,c->selected);char s[32];snprintf(s,sizeof(s),"%d Minuten",*c->sleep_timer_minutes);draw_text(c->renderer,c->font,s,20,100,c->white);draw_text(c->renderer,c->font,"Links/Rechts: -/+2min   A: Starten   B: Zurueck",20,SCREEN_H-35,c->gray);
+
+static void activate(ScreenContext *c)
+{
+    switch (selection) {
+        case 0:
+            *c->screen = SCREEN_SYSTEM_INFO;
+            break;
+        case 1:
+            *c->screen = SCREEN_BUTTON_DEBUG;
+            break;
+        case 2:
+            *c->running = 0;
+            break;
+        case 3:
+            c->shutdown_fn(c->music);
+            *c->running = 0;
+            break;
+    }
+}
+
+void sleeptimer_handle_event(ScreenContext *c,const SDL_Event *e)
+{
+    if(e->type==SDL_JOYBUTTONDOWN){
+        int b=e->jbutton.button;
+        if(b==BUTTON_B){*c->screen=SCREEN_PLAYER;return;}
+        if(b==BUTTON_Y){*c->screen=SCREEN_MENU;return;}
+        if(b==BUTTON_DPAD_UP){move_selection(-1);return;}
+        if(b==BUTTON_DPAD_DOWN){move_selection(1);return;}
+        if(b==BUTTON_A){activate(c);return;}
+    }
+
+    if(e->type==SDL_JOYAXISMOTION && e->jaxis.axis==AXIS_Y){
+        if(!*c->axis_y_lock && e->jaxis.value<-AXIS_DEADZONE){
+            move_selection(-1);
+            *c->axis_y_lock=1;
+        }else if(!*c->axis_y_lock && e->jaxis.value>AXIS_DEADZONE){
+            move_selection(1);
+            *c->axis_y_lock=1;
+        }
+        if(abs(e->jaxis.value)<AXIS_DEADZONE)*c->axis_y_lock=0;
+    }
+}
+
+void sleeptimer_render(ScreenContext *c)
+{
+    draw_text(c->renderer,c->font,"Menue",20,20,c->selected);
+
+    int y=90;
+    for(int i=0;i<ITEM_COUNT;i++){
+        SDL_Color col=(i==selection)?c->selected:c->white;
+        draw_text(c->renderer,c->font,items[i],45,y,col);
+        y+=42;
+    }
+
+    draw_text(c->renderer,c->font,
+              "A: Auswaehlen   B: Player   Y: Hoerspiele",
+              20,SCREEN_H-35,c->gray);
 }
