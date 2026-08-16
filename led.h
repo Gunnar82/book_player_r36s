d3004abf6b1b329ea51d3e68bc0e00d138ae0180
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-16 12:06:27 +0200

    LED-GPIO dynamisch konfigurierbar machen

--- led.h
+++ led.h
@@ -1,9 +1,12 @@
 #ifndef LED_H
 #define LED_H
 
-/* Schaltet die LED an (1) oder aus (0). Schreibt nur bei tatsächlicher
-   Zustandsänderung (spart unnötige Dateizugriffe). Bei fehlender
-   Schreibberechtigung wird EINMALIG eine Warnung ausgegeben. */
+void led_init(void);
 void led_set(int on);
+int led_get_gpio(void);
+int led_gpio_is_manual(void);
+int led_set_gpio_manual(int gpio);
+int led_reset_gpio_auto(void);
+int led_gpio_available(int gpio);
 
 #endif /* LED_H */
