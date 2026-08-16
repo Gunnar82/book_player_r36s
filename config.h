72c1bed60699caa4fa56844ec75b9c326b317734
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-16 15:06:59 +0200

    Version auf 0.1.12 erhöhen

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 #define APP_NAME "Hoerspiel Player"
-#define APP_VERSION "0.1.11"
+#define APP_VERSION "0.1.12"
 #define SCREEN_W 640
 #define SCREEN_H 480
 #define MAX_BOOKS 200
@@ -22,39 +22,25 @@
  * Schultertasten:
  * L1 = 4   R1 = 5
  * L2 = 6   R2 = 7
- *
- * In Listenansichten (Buecher/Tracks):
- * L1 = eine Seite zurueck     L2 = zum Anfang springen
- * R1 = eine Seite vor          R2 = zum Ende springen
- *
- * Lautstaerke:
- * Lauter = Tastaturcode 128
- * Leiser = Tastaturcode 129
  */
 #define BUTTON_A           1
 #define BUTTON_B           0
 #define BUTTON_X           2
 #define BUTTON_Y           3
 #define BUTTON_START      13
-/* Steuerkreuz */
 #define BUTTON_DPAD_UP     8
 #define BUTTON_DPAD_DOWN   9
 #define BUTTON_DPAD_LEFT  10
 #define BUTTON_DPAD_RIGHT 11
-/* Schultertasten */
 #define BUTTON_L1   4
 #define BUTTON_R1   5
 #define BUTTON_L2   6
 #define BUTTON_R2   7
-/* Select = Tastensperre aktivieren */
 #define BUTTON_SELECT 12
-/* Seitenweite fuer L1 (zurueck) / R1 (vor) in Listenansichten. */
 #define LIST_PAGE_SIZE 10
-/* Laenge der zufaelligen Tastenfolge zum Entsperren. */
 #define UNLOCK_SEQUENCE_LEN 4
 #define KEY_VOLUME_UP    128
 #define KEY_VOLUME_DOWN  129
-/* evdev: Display an/aus */
 #define EV_KEY_MID 708
 #define AXIS_X 0
 #define AXIS_Y 1
@@ -62,27 +48,15 @@
 #define SEEK_STEP 15.0
 #define SAVE_INTERVAL_MS 5000
 #define VOLUME_STEP 8
-/* Zeit ohne Eingabe, nach der das Display automatisch abschaltet.
-   Beliebig anpassbar, z.B. 30000 = 30s, 120000 = 2 Minuten. */
 #define DISPLAY_TIMEOUT_MS 60000
-/* Sleeptimer: Standardwert und Schrittweite beim Einstellen (in Minuten). */
 #define SLEEP_DEFAULT_MINUTES 30
 #define SLEEP_STEP_MINUTES     2
 #define SLEEP_MIN_MINUTES      0
 #define SLEEP_MAX_MINUTES    180
-
-/* Idle-Timer: 0 = aus. Einstellung bleibt ueber Neustarts erhalten. */
 #define IDLE_TIMER_STEP_MINUTES 1
 #define IDLE_TIMER_MAX_MINUTES 360
-/* Ab wie vielen verbleibenden Sekunden des Sleeptimers die LED blinkt. */
 #define LED_BLINK_THRESHOLD_SEC 60
-/* Blink-Geschwindigkeit (volle An/Aus-Periode) in Millisekunden. */
 #define LED_BLINK_PERIOD_MS 500
-/*
- * Linux Backlight
- */
-#define BACKLIGHT_PATH \
-    "/sys/class/backlight/backlight/brightness"
-#define BACKLIGHT_MAX_PATH \
-    "/sys/class/backlight/backlight/max_brightness"
+#define BACKLIGHT_PATH "/sys/class/backlight/backlight/brightness"
+#define BACKLIGHT_MAX_PATH "/sys/class/backlight/backlight/max_brightness"
 #endif /* CONFIG_H */
