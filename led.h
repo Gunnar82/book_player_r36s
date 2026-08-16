4a2d7bd8b148f5feb3d5eac9331046a978dbce90
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-16 12:16:24 +0200

    LED-Testmodus hinzufügen

--- led.h
+++ led.h
@@ -9,4 +9,10 @@ int led_set_gpio_manual(int gpio);
 int led_reset_gpio_auto(void);
 int led_gpio_available(int gpio);
 
+/* LED-Test: Im Testmodus werden normale LED-Anforderungen ignoriert.
+   led_test_tick() wechselt den Zustand alle 500 ms. */
+void led_set_test_mode(int enabled);
+int led_test_mode(void);
+void led_test_tick(unsigned int now_ms);
+
 #endif /* LED_H */
