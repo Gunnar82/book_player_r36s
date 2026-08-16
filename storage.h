529296e121cc80970bd9d7937116cee17c1ea83c
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-16 12:06:50 +0200

    Hardware-Konfiguration in config.ini unterstützen

--- storage.h
+++ storage.h
@@ -9,5 +9,7 @@ typedef struct { char path[STORAGE_PATH_LEN]; char label[STORAGE_LABEL_LEN]; int
 int get_storage_paths(StoragePath paths[], int max_paths);
 const char *get_audio_directory(void);
 const char *get_storage_config_path(void);
+int get_led_gpio_config(int *gpio, int *is_manual);
+int set_led_gpio_config(int gpio, int is_manual);
 
 #endif
