84464e13226de21afa16580817f90178c70ba092
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-21 10:29:06 +0200

    Release 0.2.30: pause idle timer during downloads

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 #define APP_NAME "Hoerspiel Player"
-#define APP_VERSION "0.2.29-bt-id-hotplug"
+#define APP_VERSION "0.2.30-idle-download-pause"
 #define SCREEN_W 640
 #define SCREEN_H 480
 #define MAX_BOOKS 200
