1e89c086f4fe85b77b1cecf098e4d36944d2aee8
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-24 16:44:16 +0200

    Expose playback config refactor entrypoint

--- playback_config.h
+++ playback_config.h
@@ -1,8 +1,6 @@
 #ifndef PLAYBACK_CONFIG_H
 #define PLAYBACK_CONFIG_H
 
-int playback_config_save(int repeat_book,
-                         int shutdown_after_tracks,
-                         int shutdown_at_book_end);
+int playback_config_save_current(void);
 
 #endif
