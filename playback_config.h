774fb516968c951ac1d160e9f8251bdddf55baf0
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-24 16:43:10 +0200

    Add playback config writer interface

---
+++ playback_config.h
@@ -0,0 +1,8 @@
+#ifndef PLAYBACK_CONFIG_H
+#define PLAYBACK_CONFIG_H
+
+int playback_config_save(int repeat_book,
+                         int shutdown_after_tracks,
+                         int shutdown_at_book_end);
+
+#endif
