89ad6917a7fadf770f26b6c2abb0e61eef136653
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-25 13:06:07 +0200

    Allow X removal in Batocera Bluetooth screen

--- batocera_bluetooth.h
+++ batocera_bluetooth.h
@@ -2,6 +2,7 @@
 #define BATOCERA_BLUETOOTH_H
 
 #include "bluetooth.h"
+#include <SDL2/SDL.h>
 
 int batocera_bluetooth_available(void);
 int batocera_bluetooth_enable(void);
@@ -14,5 +15,10 @@ int batocera_bluetooth_pair(const char *mac);
 int batocera_bluetooth_start_live_devices(void);
 void batocera_bluetooth_stop_live_devices(void);
 int batocera_bluetooth_live_devices(BluetoothDevice *devices,int max_devices);
+
+/* Der globale Event-Handler reserviert X normalerweise fuer das Systemmenue.
+   Im Batocera-Bluetooth-Screen wird X deshalb vor diesem Handler intern auf
+   die lokale Entfernen-Aktion umgesetzt. */
+int batocera_bluetooth_remap_event(SDL_Event *e);
 
 #endif
