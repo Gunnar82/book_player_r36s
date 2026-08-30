9fd71d54742fbf72f254cc290f3e091b8e3aab94
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-30 21:48:26 +0200

    Add self-update download interface

---
+++ update_install.h
@@ -0,0 +1,14 @@
+#ifndef UPDATE_INSTALL_H
+#define UPDATE_INSTALL_H
+
+#include "update_check.h"
+
+#define UPDATE_STAGE_PATH "/tmp/hoerspiel_player.new"
+#define UPDATE_INSTALL_STATUS_LEN 192
+
+int update_download_and_verify(const UpdateManifest *manifest,
+                               const char *target_path,
+                               char *status,
+                               int status_size);
+
+#endif
