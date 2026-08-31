e704a77614ee149b2bc0e72b672e01823b522cf0
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-08-31 08:53:28 +0200

    Add information mode to system info screen

--- screens/systeminfo.h
+++ screens/systeminfo.h
@@ -2,6 +2,7 @@
 #define SCREEN_SYSTEMINFO_H
 #include "screen_context.h"
 #define net_connection_active network_connection_active
+void systeminfo_set_information_mode(int information_mode);
 void systeminfo_handle_event(ScreenContext *ctx, const SDL_Event *e);
 void systeminfo_render(ScreenContext *ctx);
 #endif
