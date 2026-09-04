528cc1ffd1b2c1794395cd9979856f24c35898ea
Gunnar82 <83780115+Gunnar82@users.noreply.github.com>
2026-09-04 23:26:47 +0200

    Bump version to 0.3.66

--- config.h
+++ config.h
@@ -12,7 +12,7 @@
 #define FONT_PATH "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
 #endif
 
-#define APP_VERSION "0.3.65"
+#define APP_VERSION "0.3.66"
 #define SCREEN_W 640
 #define SCREEN_H 480
 #define MAX_BOOKS 200
